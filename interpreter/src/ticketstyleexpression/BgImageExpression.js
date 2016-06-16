cf820ceb8dd3d4dade061ab597a64f4342890a75
telmanagababov <telman.agababov@gmail.com>
2016-06-17 02:21:39 +0300

    update style for interpreter

--- interpreter/src/ticketstyleexpression/BgImageExpression.js
+++ interpreter/src/ticketstyleexpression/BgImageExpression.js
@@ -5,7 +5,6 @@ BgImageExpression.prototype = Object.create(StyleExpression.prototype);
 
 BgImageExpression.prototype.interpret = function (context) {
 	StyleExpression.prototype.interpret.call(this, context);
-	this._updateContext("backgroundImage", "backgroundImage");
 	var value = this._getExpression("backgroundImage");
 	this._setContextValue("backgroundImage", "url(" + value + ")");
 };
